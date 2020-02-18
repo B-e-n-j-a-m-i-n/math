4aef442576d58233bb6dfc46f2e06a4580007fdb
Benjamin AUBRY <30434893+B-e-n-j-a-m-i-n@users.noreply.github.com>
2020-02-18 12:14:14 +0100

    fix

--- php/modulo.php
+++ php/modulo.php
@@ -1,17 +1,26 @@
+<?php
 /**
- * Calcul le modulo d'un très grand nombre
- * 
- * @param string $s
- * @param int $m
+ * Calcule le modulo d'un grand nombre
+ * @param string    $s    grand nombre entier
+ * @param int       $m    modulo
+ *
  * @return number
  */
-function modulo(string $s, int $m)
-{
-    if (strlen($s) <= 15) {
+function modulo(string $s, int $m) {
+    $sizeMax = 9; // Longueur max d'entier sur système 32 bit
+    if (strlen($s) <= $sizeMax) {
         return $s % $m;
     } else {
-        $mod1 = modulo(substr($s, 15), $m);
-        $mod2 = modulo(modulo(substr($s, 0, 15), $m) * modulo(pow(10, (strlen(substr($s, 15)))), $m), $m);
-        return modulo($mod1 + $mod2, $m);
+        // partie de droite : nombre de taille sizeMax-1
+        $nb1 = substr($s, strlen($s) - ($sizeMax - 1));
+        $m1 = modulo($nb1, $m);
+        // partie de gauche : nombre de taille length - sizeMax-1
+        $nb2 = substr($s, 0,strlen($s) - ($sizeMax - 1));
+        $m2 = modulo($nb2, $m);
+        // puissance de 10 à appliquer à m2
+        $nb3 = pow(10, strlen($nb1));
+        $m3 = modulo($nb3, $m);
+        // Calcule du modulo final
+        return modulo($m1 + modulo($m2 * $m3, $m), $m);
     }
 }
