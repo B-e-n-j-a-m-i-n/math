0a41256b2e4378448119e12ff0442af1b10f8e31
Benjamin AUBRY <30434893+B-e-n-j-a-m-i-n@users.noreply.github.com>
2020-02-18 12:04:39 +0100

    Fix

--- js/modulo.js
+++ js/modulo.js
@@ -1,18 +1,27 @@
-/**
- * Calcul le modulo d'un très grand nombre
+/** 
+ * Calcule le modulo d'un grand nombre
+ * @param string    s    grand nombre entier		
+ * @param int       m    modulo
  * 
- * @param string s
- * @param int m
  * @return number
  */
 function modulo(s, m) {
+    var sizeMax = 14; // Taille max d'entier pour l'opérateur % sur IE5
     s = s.toString();
     m = parseInt(m);
-    if (s.length <= 15) {
+    if (s.length <= sizeMax) {
         return parseInt(s) % m;
     } else {
-        var mod1 = modulo(s.substr(15), m);
-        var mod2 = modulo(modulo(s.substr(0,15), m) * modulo(Math.pow(10, s.substr(15).length), m), m);
-        return modulo(mod1 + mod2, m);      
+        // partie de droite : nombre de taille sizeMax-1
+        var nb1 = s.substr(s.length - (sizeMax - 1));
+        var m1 = modulo(nb1, m);
+        // partie de gauche : nombre de taille length - sizeMax-1
+        var nb2 = s.substr(0,s.length - (sizeMax - 1));
+        var m2 = modulo(nb2, m);
+        // puissance de 10 à appliquer à m2
+        var nb3 = Math.pow(10, nb1.length);
+        var m3 = modulo(nb3, m);
+        // Calcule du modulo final
+        return modulo(m1 + modulo(m2 * m3, m), m);
     }
 }
